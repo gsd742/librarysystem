8f4277c3032bb229941273d14a3d75f017f1a688
li <gs210755@reigate.ac.uk>
2021-12-07 23:31:35 +0000

    username

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+
+import org.w3c.dom.ls.LSOutput;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.lang.invoke.SwitchPoint;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -11,15 +15,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("what would you like to do?");
         System.out.println("1. register a book");
-        int task = Integer.parseInt("enter number associated with task: ");
+        System.out.println("2. log in ");
+        System.out.println("3. register for an account");
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter number associated with task: ");
+        int task = Integer.parseInt(input.nextLine());
         switch(task){
             case(1):
-                bookdetails();
-                FileWriter mywriter =
-             //**************you were here**************************
-             //making what you receive from the method save to the external file
-
-
+                savebook();
+                break;
+            case (2):
+                System.out.println("log in");
+                break;
+            case (3):
+                System.out.println("register");
+                break;
         }
 
         }
@@ -28,16 +38,69 @@ public class Main {
 
     public static String bookdetails(){
         String booktitle = getString("enter book title:");
-        int isbn = Integer.parseInt(getString("enter the ISBN: "));
+        String isbn = getString("enter the ISBN: ");
         String author = getString("enter the author: ");
-        String genre = getString("enter the author: ");
-        return (booktitle+ "%" + isbn + "%" + author +"%"+ genre);
-        //good use of delibrators
+        String genre = getString("enter the genre: ");
+        return (booktitle+ "%" + isbn + "%" + author +"%"+ genre); //add in % to know where the things seperate from each other
     }
+
+    public static String register(){
+        boolean valid = false;
+        String username = "";
+        while (valid == false) {
+            username = getString("enter your email address: ");
+            if (username.contains("@")) {
+                valid = true;
+            }
+        }
+        valid = false ;
+        while (valid == false){
+            String password = getString("enter your password: ");
+            boolean length = false;
+            boolean upper = false;
+            boolean lower = false;
+            if (password.length()>8){
+                length = true;
+            }
+            else{
+                System.out.println("too short, at least 8 character plz");
+            }
+            if(password.matches(".*[A-Z].*")){ //check a-z capital
+                upper = true;
+            }
+            else{
+                System.out.println("need to have upper case");
+            }
+            if (password.matches(".*[a-z].*")){ // check a-z lower
+                lower = true;
+            }
+            else{
+                System.out.println("need to have lower case");
+            }
+            //*************you were here************
+            //if all of the three are true then valid is true
+            }
+
+
+
+        }
 
     public static String getString(String prompt){
         System.out.println(prompt);
         Scanner input = new Scanner(System.in);
         return input.nextLine();
     }
+
+    public static void savebook(){
+        try {
+            FileWriter mywriter = new FileWriter(books);
+            mywriter.write(bookdetails());
+            mywriter.close();
+            System.out.println("book has been successfully stored ");
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
 }
