e2c670139b80b9d4d77ec3c6497a1dce0a6b305d
li <gs210755@reigate.ac.uk>
2022-01-10 14:22:52 +0000

    i dont know

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,55 +1,72 @@
 package com.company;
 
+//made it to add them to list after beingmade objects? ns how to edit/delete them
 
 import org.w3c.dom.ls.LSOutput;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.lang.invoke.SwitchPoint;
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    public static File books = new File("books");
+    public static File storedbooks = new File("books"); //...?
+    public static ArrayList<books> BookList = new ArrayList<>();
+    public static ArrayList<borrower> BorrowerList = new ArrayList<>();
+
 
     public static void main(String[] args) {
         System.out.println("what would you like to do?");
         System.out.println("1. register a book");
         System.out.println("2. log in ");
         System.out.println("3. register for an account");
+        System.out.println("4. edit book details");
         Scanner input = new Scanner(System.in);
         System.out.println("enter number associated with task: ");
         int task = Integer.parseInt(input.nextLine());
         switch(task){
             case(1):
-                savebook();
+                bookdetails();
                 break;
             case (2):
                 System.out.println("log in");
                 break;
             case (3):
                 System.out.println("register");
+                register();
                 break;
+            case (4):
+                System.out.println("edit books");
+
+
         }
 
         }
 
 
 
-    public static String bookdetails(){
+    public static void  bookdetails(){ //gets the book details input only
         Scanner input = new Scanner(System.in);
         String booktitle = getString("enter book title:");
         System.out.println("enter the ISBN: ");
         Integer isbn = input.nextInt();
         String author = getString("enter the author: ");
         String genre = getString("enter the genre: ");
-        books newbook = new books (booktitle, isbn, author, genre);
-
-        return (booktitle+ "%" + isbn + "%" + author +"%"+ genre); //add in % to know where the things seperate from each other
+        books newbook = new books (booktitle, isbn, author, genre); //adds to object
+        BookList.add(newbook); //adds book to list
     }
 
-    public static String register(){
-        boolean valid = false;
+    public static void register(){
+      String username =getusername();
+      String password = getpassword();
+      borrower newborrower = new borrower(username, password);
+      BorrowerList.add(newborrower); //add to borrower list
+    }
+
+    public static String getusername(){
+        boolean valid = false; //username
         String username = "";
         while (valid == false) {
             username = getString("enter your email address: ");
@@ -57,9 +74,14 @@ public class Main {
                 valid = true;
             }
         }
-        valid = false ;
+        return username;
+    }
+
+    public static String getpassword(){
+        boolean valid = false ;
+        String password = " ";
         while (valid == false){
-            String password = getString("enter your password: ");
+            password = getString("enter your password: ");
             boolean length = false;
             boolean upper = false;
             boolean lower = false;
@@ -81,23 +103,34 @@ public class Main {
             else{
                 System.out.println("need to have lower case");
             }
-            //*************you were here************
-            //if all of the three are true then valid is true
+            if (length == true & upper == true & lower == true){
+                valid = true;
             }
-
-
-
         }
+        return password;
+    }
 
-    public static String getString(String prompt){
+      public static String getString(String prompt){
         System.out.println(prompt);
         Scanner input = new Scanner(System.in);
         return input.nextLine();
     }
 
+    public static void  editbook(){ //unfinished not sure how to edit
+        String title = getString("what is the name of the book?");
+        if (BookList.contains(title)== true){
+            // change what it is books.booktitle = new title
+
+        }
+        else{
+            System.out.println("title doesnt exist");
+        }
+
+    }
+
     public static void savebook(){
         try {
-            FileWriter mywriter = new FileWriter(books);
+            FileWriter mywriter = new FileWriter(storedbooks);
             mywriter.write(bookdetails());
             mywriter.close();
             System.out.println("book has been successfully stored ");
@@ -105,6 +138,6 @@ public class Main {
         catch (Exception e){
             System.out.println(e);
         }
-    }
+    } //was for saving to file but ns ? NOT USED
 
 }
